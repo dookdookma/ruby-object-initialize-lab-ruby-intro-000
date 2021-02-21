bd70931daa3251a0a4b4e4020262d539032ebbea
Luke Rollins <ldrollins@gmail.com>
2021-02-21 14:29:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
+  def breed
+    @breed
+  end
+
   def initialize(breed)
     @breed = breed
   end
 
   def breed=(breed)
     @breed = breed
-  end
-
-  def breed
-    @breed
   end
 end
